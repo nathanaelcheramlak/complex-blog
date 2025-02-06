ff9d02eefa8df932caccf398db73842f2bc46a9f
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-02-07 00:13:39 +0300

    database connection

---
+++ api/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  const DB_URI = process.env.MONGO_DB_URI || '';
+  try {
+    await mongoose.connect(DB_URI as string);
+    console.log('Database connected successfully');
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(`Error connecting to MongoDB: ${error.message}`);
+    } else {
+      console.error(`An unknown error occurred.`);
+    }
+    process.exit(1);
+  }
+};
+
+export default connectDB;

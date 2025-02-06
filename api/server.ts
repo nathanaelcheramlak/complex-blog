2fbe25fd72500166b5c5248c35e37c4f33a6c019
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-06 22:28:47 +0300

    Added swagger

--- api/server.ts
+++ api/server.ts
@@ -1,8 +1,33 @@
-import express from 'express'
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerDocument from './swagger.json';
 
-const app = express()
+const app = express();
+
+const options = {
+  swaggerDefinition: swaggerDocument,
+  apis: ['./server.ts'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Welcome message
+ *     responses:
+ *       200:
+ *         description: Returns a welcome message.
+ */
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
 
 // Start the Server
 app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:${3000}`);
+  console.log(`Server is running on http://localhost:${3000}`);
 });

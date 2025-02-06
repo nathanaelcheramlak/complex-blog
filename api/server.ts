f8e928e6f4b0e9e8bc872e52b5b3650b90d701c8
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-02-07 01:02:46 +0300

    config and routes

--- api/server.ts
+++ api/server.ts
@@ -1,19 +1,46 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerDocument from './swagger.json';
 
 import connectDB from './config/db';
+import {
+  authRoutes,
+  userRoutes,
+  blogRoutes,
+  bookmarkRoutes,
+  commentRoutes,
+  likeRoutes,
+} from './routes/routes';
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Config
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// CORS Config
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  }),
+);
+
+// Swagger Config
 const options = {
   swaggerDefinition: swaggerDocument,
   apis: ['./server.ts'], // Path to the API docs
 };
-
 const specs = swaggerJsdoc(options);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 /**
@@ -29,8 +56,16 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API');
 });
 
+// Routes
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/blogs', blogRoutes);
+app.use('/bookmarks', bookmarkRoutes);
+app.use('/comments', commentRoutes);
+app.use('/likes', likeRoutes);
+
 // Start the Server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on http://localhost:${3000}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });

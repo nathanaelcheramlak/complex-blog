be8306fd2bdd404be6146c28337ba69f4806f4e4
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-02-07 22:47:36 +0300

    route implemented for auth and user

--- api/routes/authRoutes.ts
+++ api/routes/authRoutes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import {
+  login,
+  register,
+  forgotPassword,
+  logout,
+  me,
+} from '../controller/authController';
 
 const app = express.Router();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'GET Auth' });
-});
+app.post('/login', login);
+app.post('/register', register);
+app.post('/forgot-password', forgotPassword);
+app.delete('/logout', logout);
+app.get('/me', me);
 
 export default app;

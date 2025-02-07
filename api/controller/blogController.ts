9b9e71fb03955727d5ef2bb090f47c1a5f657316
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-07 23:13:46 +0300

    async controllers

--- api/controller/blogController.ts
+++ api/controller/blogController.ts
@@ -1,21 +1,21 @@
-import { Request, RequestHandler, Response } from 'express';
+import { Request, Response } from 'express';
 
-export const getBlogs = (request: Request, response: Response) => {
+export const getBlogs = async (request: Request, response: Response) => {
   response.status(200).send([]);
 };
 
-export const getBlogById = (request: Request, response: Response) => {
+export const getBlogById = async (request: Request, response: Response) => {
   response.status(200).json({});
 };
 
-export const createBlog = (request: Request, response: Response) => {
+export const createBlog = async (request: Request, response: Response) => {
   response.status(200).json({});
 };
 
-export const updateBlog = (request: Request, response: Response) => {
+export const updateBlog = async (request: Request, response: Response) => {
   response.status(200).json({});
 };
 
-export const deleteBlog = (request: Request, response: Response) => {
+export const deleteBlog = async (request: Request, response: Response) => {
   response.status(200).json({});
 };
--- api/controller/likeController.ts
+++ api/controller/likeController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 
-export const getLikes = (request: Request, response: Response) => {
+export const getLikes = async (request: Request, response: Response) => {
   response.status(200).send([]);
 };
 
-export const createLike = (request: Request, response: Response) => {
+export const createLike = async (request: Request, response: Response) => {
   response.status(201).json({});
 };
 
-export const deleteLike = (request: Request, response: Response) => {
+export const deleteLike = async (request: Request, response: Response) => {
   response.status(200).json({});
 };

1f84940255bc4fcf4e19080c2399bc68f03297cb
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-19 21:29:21 +0300

    fix validator

--- api/middlewares/validateRegister.ts
+++ api/middlewares/validateRegister.ts
@@ -1,21 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
-import { body } from 'express-validator';
-import { CreateUserDTO } from '../dtos/CreateUser.dto';
+import { body } from "express-validator";
 
-export const registerValidateMiddleware = async (
-  req: Request<{}, {}, CreateUserDTO>,
-  res: Response,
-  next: NextFunction,
-) => {
+export const registerValidateMiddleware = [
   // Validation
-  body('fullname').notEmpty().withMessage('Fullname is required');
-  body('email').trim().isEmail().withMessage('Invalid email');
-  body('username').notEmpty().withMessage('Username is required');
-  body('password')
+  body("fullname").notEmpty().withMessage("Fullname is required"),
+  body("email").trim().isEmail().withMessage("Invalid email"),
+  body("username").notEmpty().withMessage("Username is required"),
+  body("password")
     .isLength({ min: 6 })
     .isStrongPassword()
-    .withMessage('Password must be 6 characters or more');
-  body('dateOfBirth').isDate().withMessage('Invalid date of birth');
-
-  next();
-};
+    .withMessage("Password must be 6 characters or more"),
+  body("dateOfBirth").isDate().withMessage("Invalid date of birth"),
+];

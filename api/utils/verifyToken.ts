2e1dee33a1ee0fe93867f1854a2b729d99ce6ff3
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-02-19 20:28:27 +0300

    verify token minor update

--- api/utils/verifyToken.ts
+++ api/utils/verifyToken.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { UserPayload } from '../types/UserPayloads';
 
-// Extend the Request interface to include user data
 interface CustomRequest extends Request {
   user?: UserPayload;
 }
@@ -12,25 +11,22 @@ export const authenticateJWT = async (
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
-  // Get the token from the Authorization header (Bearer <token>)
-  const token = req.headers.cookie?.split('=')[1];
-
-  // Check if the token exists
-  if (!token) {
-    res.status(401).json({ message: 'Unauthorized', error: true });
-    return;
-  }
-
   try {
-    // Verify the token
+    const token = req.cookies?.auth_token;
+
+    if (!token) {
+      res.status(401).json({ message: 'Unauthorized', error: true });
+      return;
+    }
+
     const user = jwt.verify(
       token,
       process.env.JWT_SECRET as string,
     ) as UserPayload;
+
     req.user = user;
     next();
   } catch (err) {
     res.status(403).json({ message: 'Invalid or expired token', error: true });
-    return;
   }
 };

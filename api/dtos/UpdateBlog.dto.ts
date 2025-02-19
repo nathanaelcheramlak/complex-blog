5d5db02746c356940907b84a83445a572963fc08
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-19 22:37:29 +0300

    created UpdateBlog dto and moved all exports to index.ts

---
+++ api/dtos/UpdateBlog.dto.ts
@@ -0,0 +1,4 @@
+export type UpdateBlogDto = {
+  title?: string;
+  content?: string;
+};
---
+++ api/dtos/index.ts
@@ -0,0 +1,5 @@
+import { UpdateBlogDto } from './UpdateBlog.dto';
+import { CreateBlogDto } from './CreateBlog.dto';
+import { CreateUserDTO } from './CreateUser.dto';
+
+export { UpdateBlogDto, CreateBlogDto, CreateUserDTO };

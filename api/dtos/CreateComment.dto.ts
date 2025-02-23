24e350bef9c0388d130736bde2306fba1a7d5483
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-23 23:04:24 +0300

    Created Comment DTO

---
+++ api/dtos/CreateComment.dto.ts
@@ -0,0 +1,3 @@
+export type CreateCommentDto = {
+  comment: string;
+};
--- api/dtos/index.ts
+++ api/dtos/index.ts
@@ -1,5 +1,6 @@
 import { UpdateBlogDto } from './UpdateBlog.dto';
 import { CreateBlogDto } from './CreateBlog.dto';
 import { CreateUserDTO } from './CreateUser.dto';
+import { CreateCommentDto } from './CreateComment.dto';
 
-export { UpdateBlogDto, CreateBlogDto, CreateUserDTO };
+export { UpdateBlogDto, CreateBlogDto, CreateUserDTO, CreateCommentDto };

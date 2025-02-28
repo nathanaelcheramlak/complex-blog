858d70e73772c57abb82143d8dad6dd32e3783e4
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-28 22:35:40 +0300

    Remove blog from user.blogs

--- api/controller/blogController.ts
+++ api/controller/blogController.ts
@@ -13,7 +13,7 @@ import User from '../models/user';
 
 export const getBlogs = async (
   request: Request<{}, {}, {}, SortByQuery>,
-  response: Response<BlogTypeSorted[] | ErrorType>,
+  response: Response<BlogTypeSorted[] | ErrorType>
 ) => {
   try {
     const { sortBy, order } = request.query;
@@ -48,7 +48,7 @@ export const getBlogs = async (
 
 export const getBlogById = async (
   request: Request,
-  response: Response<PopulatedBlogType | ErrorType>,
+  response: Response<PopulatedBlogType | ErrorType>
 ) => {
   const { id } = request.params;
   try {
@@ -74,7 +74,7 @@ export const getBlogById = async (
 
 export const createBlog = async (
   request: CustomRequest<{}, {}, CreateBlogDto>,
-  response: Response<BlogType | ErrorType>,
+  response: Response<BlogType | ErrorType>
 ) => {
   const userId = request.user?.id;
   const { title, content } = request.body;
@@ -120,7 +120,7 @@ export const createBlog = async (
 
 export const updateBlog = async (
   request: CustomRequest<{ id: string }, {}, UpdateBlogDto>,
-  response: Response<BlogType | ErrorType>,
+  response: Response<BlogType | ErrorType>
 ) => {
   const userId = request.user?.id;
   const { id } = request.params;
@@ -171,7 +171,7 @@ export const updateBlog = async (
 
 export const deleteBlog = async (
   request: CustomRequest<{ id: string }, {}, {}>,
-  response: Response<{ message: string } | ErrorType>,
+  response: Response<{ message: string } | ErrorType>
 ) => {
   const userId = request.user?.id;
   const { id } = request.params;
@@ -183,6 +183,7 @@ export const deleteBlog = async (
 
   try {
     const blog = await Blog.findById(id);
+    const user = await User.findById(userId);
 
     if (!blog) {
       response.status(404).json({ message: 'Blog not found' });
@@ -192,6 +193,11 @@ export const deleteBlog = async (
     if (blog.author.toString() !== userId) {
       response.status(403).json({ message: 'You are not authorized' });
       return;
+    }
+
+    if (user) {
+      user.blogs = user.blogs.filter((blogId) => blogId.toString() !== id);
+      await user.save();
     }
 
     await blog.deleteOne();

429c2dc6861abc7fe31770aaf3912169c530c167
DanielJohn17 <danielyohannes38@gmail.com>
2025-02-28 23:15:01 +0300

    finished getLikes controller

--- api/controller/likeController.ts
+++ api/controller/likeController.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from 'express';
+import Blog from '../models/blog';
+import User from '../models/user';
+import type { ErrorType, LikeTypeSorted } from '../types/response';
+import type { CustomRequest } from '../types/request';
+import Like from '../models/like';
 
-export const getLikes = async (request: Request, response: Response) => {
-  response.status(200).send([]);
+export const getLikes = async (
+  request: Request<{ blogId: string }>,
+  response: Response<{ likes: number } | ErrorType>
+) => {
+  const { blogId } = request.params;
+
+  try {
+    const blog = await Blog.findById(blogId);
+
+    if (!blog) {
+      response.status(404).json({ message: 'Blog not found' });
+      return;
+    }
+
+    const likes = blog.likes.length;
+
+    response.status(200).json({ likes });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      response.status(500).json({ message: error.message });
+      return;
+    } else {
+      response.status(500).json({ message: 'Something went wrong' });
+      return;
+    }
+  }
 };
 
 export const createLike = async (request: Request, response: Response) => {

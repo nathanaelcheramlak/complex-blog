0d2374efb716d04bdd068fb58fe50d4d274b38b4
DanielJohn17 <danielyohannes38@gmail.com>
2026-02-16 00:57:39 +0300

    Fix: sqlite file path issue

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,22 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'node:path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['../../../.env', '../.env'],
+      envFilePath: ['../../.env', '../.env'],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'sqlite',
-        database: configService.get<string>('DATABASE_NAME', 'database.sqlite'),
-        autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_SYNCHRONIZE', true),
-      }),
+      useFactory: (configService: ConfigService) => {
+        const dbName = configService.get<string>(
+          'DATABASE_NAME',
+          'database.db',
+        );
+
+        // This points to: eg:- ./apps/api/../../complex_blog.db (the root)
+        const dbPath = join(__dirname, '..', '..', '..', dbName);
+        return {
+          type: 'sqlite',
+          database: dbPath,
+          autoLoadEntities: true,
+          synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        };
+      },
     }),
   ],
   controllers: [AppController],

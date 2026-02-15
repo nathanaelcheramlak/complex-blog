699555be41ea5537091fb76316512dfbbda512a4
DanielJohn17 <danielyohannes38@gmail.com>
2026-02-15 23:55:26 +0300

    Fix .env file path

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,7 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['../../../.env', '../.env'],
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
